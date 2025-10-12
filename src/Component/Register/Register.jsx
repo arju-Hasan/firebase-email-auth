9ee2af9f0cae764b1ce9d37da79cc8bcd84f86e2
Arju-Hasan <rajuahmeed698@gmail.com>
2025-10-13 04:07:34 +0600

    forgotPassword section add

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,71 +1,104 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
-import { auth } from '../../Firebase/Firebase.init';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { Link } from 'react-router';
+import { auth } from '../../Firebase/Firebase.init';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const [success, setSuccess] = useState(false);
-     const [Error, setError] = useState();
-     const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-    const HandelSubmit = (event)=>{
-        event.preventDefault();
+    const handleSignUp = e => {
+        e.preventDefault();
 
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        const terms = event.target.terms.checked;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
+        const terms = e.target.terms.checked;
 
-        console.log("register",  email, password, terms);
+        console.log(email, password, name, photo, terms);
 
-        setError("");
+        // reset error and status
+        setErrorMessage('');
         setSuccess(false);
 
-           if (!terms) {
-            setError('Please accept Our terms and conditions.');
-            return;}
+        if (!terms) {
+            setErrorMessage('Please accept Our terms and conditions.');
+            return;
+        }
 
-          if (password.length < 6) {
-            setError('Password should be 6 characters or longer');
+        if (password.length < 6) {
+            setErrorMessage('Password should be 6 characters or longer');
             return;
         }
 
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
 
         if (!passwordRegex.test(password)) {
-            setError('At least one uppercase, one lowercase, one number, one special character');
+            setErrorMessage('At least one uppercase, one lowercase, one number, one special character');
             return;
         }
 
-        createUserWithEmailAndPassword( auth, email, password)
-        .then(result=>{
-            console.log("user resutl" , result.user);
-            setSuccess(true)
-            event.target.reset()
+        // create user with email and password
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+                setSuccess(true);
 
-        } )
-        .catch(error=>{
-            console.log(error.message);
-            setError(error.message)
-        })
+                // send verification email address
+                sendEmailVerification(auth.currentUser)
+                    .then(() => {
+                        console.log('verification email sent')
+                    });
+
+                // update Profile name and photo url
+                const profile = {
+                    displayName: name,
+                    photoURL: photo
+                }
+                updateProfile(auth.currentUser, profile)
+                    .then(() => {
+                        console.log('user profile updated')
+                    })
+                    .catch(eror => console.log('User profile update error'));
+
+            })
+            .catch(error => {
+                console.log('ERROR', error.message);
+                setErrorMessage(error.message)
+                setSuccess(false);
+            })
+
     }
 
     return (
-        <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-     <form onSubmit={HandelSubmit}>
-           <fieldset className="fieldset">
-          <label className="label" >Email</label>
-          <input type="email" className="input" name='email' placeholder="Email" />
-          <div className="form-control relative">
+        <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
+            <h3 className="text-3xl ml-4 font-bold">Sign Up now!</h3>
+            <form onSubmit={handleSignUp} className="card-body">
+                <div className="form-control">
                     <label className="label">
-                        Password
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Photo URL</span>
+                    </label>
+                    <input type="text" name='photo' placeholder="photo url" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                </div>
+                <div className="form-control relative">
+                    <label className="label">
+                        <span className="label-text">Password</span>
                     </label>
                     <input
                         type={showPassword ? 'text' : 'password'}
@@ -74,12 +107,14 @@ const Register = () => {
                         className="input input-bordered" required />
                     <button
                         onClick={() => setShowPassword(!showPassword)}
-                        className='btn btn-xs absolute right-5 top-7'>
+                        className='btn btn-xs absolute right-2 top-12'>
                         {
                             showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
                         }
                     </button>
-                    
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
                 </div>
                 <div className="form-control">
                     <label className="label justify-start cursor-pointer">
@@ -87,24 +122,20 @@ const Register = () => {
                         <span className="label-text ml-2">Accept Our Terms And Condition.</span>
                     </label>
                 </div>
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Register</button>
-        </fieldset>
-        {
-           success  && <p className='text-green-500'>Acount successfully</p>
-        }
-         {
-        Error && <p className='text-red-500 p-4 text-center'>{Error}</p>
-     }
-     </form>
-    
-      </div>
-      <p className='m-2'>
-                Already have an account? Please <Link to="/login" className='text-blue-600 text-underline'>Login</Link>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Sign Up</button>
+                </div>
+            </form>
+            {
+                errorMessage && <p className='text-red-600'>{errorMessage}</p>
+            }
+            {
+                success && <p className='text-green-600'>Sign up is Successful.</p>
+            }
+            <p className='m-2'>
+                Already have an account? Please <Link to="/login">Login</Link>
             </p>
-    </div>
-  </div>
-</div>
+        </div>
     );
 };
 
