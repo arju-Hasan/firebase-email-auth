79cd8ca1f5d21e642101c3c087c60d67f1a46c23
Arju-Hasan <rajuahmeed698@gmail.com>
2025-10-13 03:47:35 +0600

    Register ok

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,19 +1,41 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../Firebase/Firebase.init';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from 'react-router';
 
 const Register = () => {
     const [success, setSuccess] = useState(false);
      const [Error, setError] = useState();
+     const [showPassword, setShowPassword] = useState(false);
 
     const HandelSubmit = (event)=>{
         event.preventDefault();
+
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log("register",  email, password);
+        const terms = event.target.terms.checked;
+
+        console.log("register",  email, password, terms);
 
         setError("");
         setSuccess(false);
+
+           if (!terms) {
+            setError('Please accept Our terms and conditions.');
+            return;}
+
+          if (password.length < 6) {
+            setError('Password should be 6 characters or longer');
+            return;
+        }
+
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+        if (!passwordRegex.test(password)) {
+            setError('At least one uppercase, one lowercase, one number, one special character');
+            return;
+        }
 
         createUserWithEmailAndPassword( auth, email, password)
         .then(result=>{
@@ -41,13 +63,35 @@ const Register = () => {
            <fieldset className="fieldset">
           <label className="label" >Email</label>
           <input type="email" className="input" name='email' placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" name='password' placeholder="Password" />
+          <div className="form-control relative">
+                    <label className="label">
+                        Password
+                    </label>
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        name='password'
+                        placeholder="password"
+                        className="input input-bordered" required />
+                    <button
+                        onClick={() => setShowPassword(!showPassword)}
+                        className='btn btn-xs absolute right-5 top-7'>
+                        {
+                            showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                        }
+                    </button>
+                    
+                </div>
+                <div className="form-control">
+                    <label className="label justify-start cursor-pointer">
+                        <input type="checkbox" name='terms' className="checkbox" />
+                        <span className="label-text ml-2">Accept Our Terms And Condition.</span>
+                    </label>
+                </div>
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Register</button>
         </fieldset>
         {
-           success  && <p className='text-green-500'>success</p>
+           success  && <p className='text-green-500'>Acount successfully</p>
         }
          {
         Error && <p className='text-red-500 p-4 text-center'>{Error}</p>
@@ -55,6 +99,9 @@ const Register = () => {
      </form>
     
       </div>
+      <p className='m-2'>
+                Already have an account? Please <Link to="/login" className='text-blue-600 text-underline'>Login</Link>
+            </p>
     </div>
   </div>
 </div>
